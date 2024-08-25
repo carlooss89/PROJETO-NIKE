93633002baba547ef1c0a2e683c4f47ed520c91a
Bowie <carloseduardosousas18@gmail.com>
2024-08-25 11:23:03 -0300

    Modificações no html, css e js.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
-let ul = document.querySelector("nav .ul");
+let menu = document.querySelector("nav .menu");
 
-function openMenu() /*() chama a função openMenu e executa o código dentro dela*/ {
-  ul.classList.add("open");
+function openMenu() /* Função responsável por abrir o MENU.  
+() chama a função openMenu e executa o código dentro dela*/ {
+  menu.classList.add("open");
 }
 
 function closeMenu() /*() chama a função closeMenu e executa o código dentro dela*/ {
-  ul.classList.remove("open");
+  menu.classList.remove("open");
 }
+
+// SCRIPT
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    830: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
